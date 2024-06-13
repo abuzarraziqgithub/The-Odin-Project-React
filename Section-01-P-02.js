1530330154c5ae934e79c30cdc68741c03925190
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-06-13 22:11:37 +0500

    updated

---
+++ Section-01-P-02.js
@@ -0,0 +1,20 @@
+//* Introduction to React
+
+//? What is React?
+//* According to React website, React is "The library for web and native Interfaces".
+
+//* A JavaScript Library is a collection of pre-written code that's desinged to make development easier. This code can be reused/reimplemented in our own codebases to achieve more complex tasks.
+
+//* React provides powerful primitives(built-in functions/modules) that allow us to build user interfaces of varying/different complexities.
+
+//* React is the most powerful, widely used JavaScript libraries.
+
+//* Frameworks make our code easily scalable, more readable, and possibly a thousand times more efficient.
+
+//? Few reasons to learn React
+//* -> Components are reusable
+//* -> It's well-supported due to its popularity and large community.
+//* -> It's opinionated, which means it won't force to follow any specific design patterns, project organizational structure, or logic.
+//* -> There is a samller learning curve, especially if already have a good grasp of JavaScript, HTML, and CSS from our previous lessons.
+
+// React by Odin Project
