7102491442b93959255f4bea53ac588c506b891c
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-06-16 05:18:57 +0500

    section-02-file-added

---
+++ Notes/Section-02.js
@@ -0,0 +1,14 @@
+//! Getting Started With React
+
+//? Topics Covered
+//* React Components
+//* What is JSX
+//* Rendering Techniques
+//* Keys in React
+//* Passing Data Between Components.
+
+//* 1:- COMPONENTS
+/*
+*
+
+*/
