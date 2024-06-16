041efe169096522039afd33b3f24b554f2a3519c
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-06-16 06:34:38 +0500

    1st-commit

--- Notes/Section-02.js
+++ Notes/Section-02.js
@@ -1,4 +1,4 @@
-//! Getting Started With React
+// Getting Started With React
 
 //? Topics Covered
 //* React Components
@@ -7,8 +7,16 @@
 //* Keys in React
 //* Passing Data Between Components.
 
-//* 1:- COMPONENTS
+//! 1:- COMPONENTS
 /*
-*
+? What are Components?
+* The beauty of React is it allows you to break the UI down into independent reusable chunks, which will refer to as components.
+* Think of these reusable chunks as JavaScript functions which can take some kind of input and return a React Element.
 
+? How to create Components?
+* We created a functional Greeting Component in Components Folder inside the Project.
+* The React components must be capitalized or they will not function as expected. which is why we capitalized Greeting().
+? Why we should capitalized our React component?
+* When the JSX is parsed, React uses the capitalization to tell the difference between an HTML tag and instance of a React component.
+* <greeting /> would be interpreted as a normal HTML element with no special meaning, instead of your shiny new React component. 
 */
---
+++ The-Odin-React/src/components/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1> &quot; Wohoo, I've Created my first React Component &quot;</h1>;
+}
+
+export default Greeting;
